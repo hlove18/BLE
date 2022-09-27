e5cf0ca2da218256b55b1fe993163c9ffbbe5ad8
bibit <ebianchini.mit@gmail.com>
2022-09-27 11:35:51 -0400

    Reverse order of posts in individual pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,17 +9,17 @@ class PagesController < ApplicationController
   #individual pages:
   def bibit
     if user_signed_in? and current_user.admin
-      @posts = Post.where(:owner => 'Bianchini').minimal_view.order('id')
+      @posts = Post.where(:owner => 'Bianchini').minimal_view.order('id').reverse_order
     else
-      @posts = Post.where(:owner => 'Bianchini').minimal_view.published
+      @posts = Post.where(:owner => 'Bianchini').minimal_view.published.order('id').reverse_order
     end
   end
 
   def henry
     if user_signed_in? and current_user.admin
-      @posts = Post.where(:owner => 'Love').minimal_view.order('id')
+      @posts = Post.where(:owner => 'Love').minimal_view.order('id').reverse_order
     else
-      @posts = Post.where(:owner => 'Love').minimal_view.published
+      @posts = Post.where(:owner => 'Love').minimal_view.published.order('id').reverse_order
     end
   end
 
